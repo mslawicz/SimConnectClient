605b343f2aff529211e14e72c205ad90f4ed16e0
Marcin Sławicz <marcin.slawicz@gmail.com>
2024-10-04 23:34:43 +0200

    SimConnect command compilation OK

--- src/SimConnectClient.cpp
+++ src/SimConnectClient.cpp
@@ -1,7 +1,16 @@
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <strsafe.h>
+
+#include "SimConnect.h"
 #include <iostream>
+
+HANDLE hSimConnect = NULL;
 
 int main(void)
 {
+    SimConnect_Open(&hSimConnect, "Throttle Control", NULL, 0, 0, 0);   //example of a command
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
